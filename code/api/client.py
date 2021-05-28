6c3bb2d98b76312c328f03deb82b31729ec67b25
oshynk <oshynk@softserveinc.com>
2021-05-28 13:22:48 +0300

    [CCTRI-2457] Fix flake8

--- code/api/client.py
+++ code/api/client.py
@@ -1,11 +1,9 @@
-import base64
-from http import HTTPStatus
+from base64 import b64encode
 
 import requests
 from requests.exceptions import SSLError
 
 from api.errors import (
-    AuthorizationError,
     SumoLogicSSLError,
     CriticalSumoLogicResponseError
 )
@@ -21,21 +19,20 @@ class SumoLogicClient:
 
     @property
     def _headers(self):
-        encoded = base64.b64encode(f'{self._credentials.get("accessId")}:{self._credentials.get("accessKey")}'.encode())
+        encoded = b64encode(f'{self._credentials.get("accessId")}:'
+                            f'{self._credentials.get("accessKey")}'.encode())
         return {'Authorization': f'Basic {encoded.decode()}'}
 
     def health(self):
         return self._request('healthEvents')
 
-    def _request(
-            self, path, method='GET', body=None, params=None, data_extractor=lambda r: r.json()
-    ):
+    def _request(self, path, method='GET',
+                 body=None, params=None, data_extractor=lambda r: r.json()):
         url = '/'.join([self._url, path.lstrip('/')])
 
         try:
-            response = requests.request(
-                method, url, headers=self._headers, json=body, params=params
-            )
+            response = requests.request(method, url, headers=self._headers,
+                                        json=body, params=params)
         except SSLError as error:
             raise SumoLogicSSLError(error)
 
