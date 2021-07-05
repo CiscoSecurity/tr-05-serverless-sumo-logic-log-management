597c4b2987393fcdc61cd5d43aca5ba806bfac65
Olena Shynkarenko <oshynk@softserveinc.com>
2021-07-05 17:19:23 +0300

    [CCTRI-2511] Mapping restructuring

--- code/api/mapping.py
+++ code/api/mapping.py
@@ -8,17 +8,12 @@ JUDGEMENT = 'judgement'
 VERDICT = 'verdict'
 
 SOURCE = 'Sumo Logic'
-
-CONFIDENCE = {
-    'confidence': 'High'
-}
-SCHEMA_VERSION = {
-    'schema_version': '1.0.6'
-}
+CONFIDENCE = 'High'
+SCHEMA_VERSION = '1.0.6'
 
 SIGHTING_DEFAULTS = {
-    **SCHEMA_VERSION,
-    **CONFIDENCE,
+    'confidence': CONFIDENCE,
+    'schema_version': SCHEMA_VERSION,
     'source': SOURCE,
     'type': SIGHTING,
     'title': 'Log message from last 30 days in Sumo Logic contains '
@@ -26,8 +21,8 @@ SIGHTING_DEFAULTS = {
     'internal': True
 }
 JUDGEMENT_DEFAULTS = {
-    **SCHEMA_VERSION,
-    **CONFIDENCE,
+    'confidence': CONFIDENCE,
+    'schema_version': SCHEMA_VERSION,
     'source': SOURCE,
     'type': JUDGEMENT,
     'priority': 85,
@@ -39,11 +34,42 @@ VERDICT_DEFAULTS = {
     'type': VERDICT
 }
 
+DISPOSITION_MAP = {
+    'high': {
+        'disposition': 2,
+        'disposition_name': 'Malicious'
+    },
+    'medium': {
+        'disposition': 2,
+        'disposition_name': 'Malicious'
+    },
+    'low': {
+        'disposition': 3,
+        'disposition_name': 'Suspicious'
+    },
+    'unverified': {
+        'disposition': 5,
+        'disposition_name': 'Unknown'
+    }
+}
+
+SEVERITY_MAP = {
+    'high': 'High',
+    'medium': 'Medium',
+    'low': 'Low',
+    'unverified': 'Unknown'
+}
+
+
+def valid_time(start_time, observable_type):
+    if observable_type in ['domain', 'email', 'ip', 'ipv6', 'url']:
+        return start_time + 30 * 24 * 60 * 60
+    return datetime(2525, 1, 1)
+
 
 class Sighting:
     def _sighting(self, message, observable):
         sighting = {
-            **SIGHTING_DEFAULTS,
             'count': self._count(message),
             'description': f'```\n{message.get("_raw")}\n```',
             'short_description': self._short_description(message),
@@ -55,7 +81,8 @@ class Sighting:
             'observed_time': {
                 'start_time': self._start_time(message)
             },
-            'data': self._data_table(message)
+            'data': self._data_table(message),
+            **SIGHTING_DEFAULTS
         }
 
         if message.get('src_ip') and message.get('dest_ip'):
@@ -115,52 +142,21 @@ class Sighting:
         return sighting
 
 
-class EntityPropsMixin:
-    @staticmethod
-    def _disposition(cs_data):
-        confidence = cs_data['malicious_confidence']
-        disposition_map = {
-            'high': {
-                'disposition': 2,
-                'disposition_name': 'Malicious'
-            },
-            'medium': {
-                'disposition': 2,
-                'disposition_name': 'Malicious'
-            },
-            'low': {
-                'disposition': 3,
-                'disposition_name': 'Suspicious'
-            },
-            'unverified': {
-                'disposition': 5,
-                'disposition_name': 'Unknown'
-            }
-        }
-        return disposition_map[confidence]
-
-    @staticmethod
-    def _valid_time(start_time, observable_type):
-        if observable_type in ['domain', 'email', 'ip', 'ipv6', 'url']:
-            return start_time + 30 * 24 * 60 * 60
-        return '2525-01-01T00:00:00.000Z'
-
-
-class Judgement(EntityPropsMixin):
+class Judgement():
     def _judgement(self, cs_data, observable):
         judgement = {
-            **JUDGEMENT_DEFAULTS,
-            **self._disposition(cs_data),
+            **DISPOSITION_MAP[cs_data['malicious_confidence']],
             'id': self._transient_id(cs_data, observable),
             'observables': [observable],
-            'severity': self._severity(cs_data),
+            'severity': SEVERITY_MAP[cs_data['malicious_confidence']],
             'valid_time': {
                 'start_time': cs_data['last_updated'],
-                'end_time': self._valid_time(cs_data['last_updated'],
-                                             observable['type'])
+                'end_time': valid_time(cs_data['last_updated'],
+                                       observable['type'])
             },
             'external_references': self._external_references(cs_data),
-            'source_uri': current_app.config['SUMO_API_ENDPOINT']
+            'source_uri': current_app.config['SUMO_API_ENDPOINT'],
+            **JUDGEMENT_DEFAULTS,
         }
         return judgement
 
@@ -177,19 +173,10 @@ class Judgement(EntityPropsMixin):
         return references
 
     @staticmethod
-    def _severity(cs_data):
-        confidence = cs_data['malicious_confidence']
-        severity_map = {
-            'high': 'High',
-            'medium': 'Medium',
-            'low': 'Low',
-            'unverified': 'Unknown'
-        }
-        return severity_map[confidence]
-
-    def _transient_id(self, cs_data, observable):
-        seeds = f'{SOURCE}|{observable["value"]}|' \
-                f'{self._disposition(cs_data)["disposition"]}|' \
+    def _transient_id(cs_data, observable):
+        disposition = DISPOSITION_MAP[cs_data['malicious_confidence']][
+            "disposition"]
+        seeds = f'{SOURCE}|{observable["value"]}|{disposition}|' \
                 f'{cs_data["last_updated"]}'
         judgement_id = f'transient:judgement-{uuid5(NAMESPACE_X500, seeds)}'
         return judgement_id
@@ -199,17 +186,17 @@ class Judgement(EntityPropsMixin):
         return judgement
 
 
-class Verdict(EntityPropsMixin):
+class Verdict():
     def _verdict(self, cs_data, observable):
         verdict = {
-            **self._disposition(cs_data),
-            **VERDICT_DEFAULTS,
+            **DISPOSITION_MAP[cs_data['malicious_confidence']],
             'observables': [observable],
             'valid_time': {
                 'start_time': cs_data['last_updated'],
-                'end_time': self._valid_time(cs_data['last_updated'],
-                                             observable['type'])
-            }
+                'end_time': valid_time(cs_data['last_updated'],
+                                       observable['type'])
+            },
+            **VERDICT_DEFAULTS
         }
         return verdict
 
