941ceb71f0a9fb4bc16d9db86814c7f994014fe1
Mario Aquino <mario.e.aquino@gmail.com>
2025-03-12 07:18:37 -0500

    Prefix sighting ids with 'transient:' (#41)
    * [XDR-21932] Prefix sighting ids with 'transient:'
    * [XDR-22278] Update, rebuild and redeploy integrations
    
    add actions, Makefile, add black
    update linter
    
    ---------
    
    Co-authored-by: Serge Kutwicki <skutv@softserveinc.com>

--- code/api/health.py
+++ code/api/health.py
@@ -2,12 +2,12 @@ from flask import Blueprint
 from api.utils import get_credentials, jsonify_data
 from api.client import SumoLogicClient
 
-health_api = Blueprint('health', __name__)
+health_api = Blueprint("health", __name__)
 
 
-@health_api.route('/health', methods=['POST'])
+@health_api.route("/health", methods=["POST"])
 def health():
     credentials = get_credentials()
     client = SumoLogicClient(credentials)
     _ = client.health()
-    return jsonify_data({'status': 'ok'})
+    return jsonify_data({"status": "ok"})
